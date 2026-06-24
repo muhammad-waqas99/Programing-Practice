afb005957144c0963a3bacf34e7c70307f2bad3f
Muhammad Waqas <muhammadwaqas.dev99@gmail.com>
2026-06-24 23:36:00 +0500

    Add loop exercises and Pascal triangle pattern

---
+++ CPP/Loops/factorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long long fact = 1;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n < 0) {
+        cout << "Factorial not defined for negative numbers" << endl;
+    } 
+    else {
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        cout << "Factorial = " << fact << endl;
+    }
+
+    return 0;
+}
